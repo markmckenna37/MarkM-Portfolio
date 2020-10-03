cac5840188dcdc05fb064c44280238fd5c1a7d7a
Mark <youremail@gmail.comg>
2020-10-03 15:22:43 -0600

    Updates

---
+++ precache-manifest.4b5ee5facbad2dbbd7094c507a6cc0ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b050e80447893484630a6de5933357a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fa43dbac917848652a7",
+    "url": "/static/css/2.60dcd141.chunk.css"
+  },
+  {
+    "revision": "2bbb342f39ce9e1db024",
+    "url": "/static/css/main.bbb84e94.chunk.css"
+  },
+  {
+    "revision": "4fa43dbac917848652a7",
+    "url": "/static/js/2.848de250.chunk.js"
+  },
+  {
+    "revision": "f4edca991a879da102dc4a0be79ba17c",
+    "url": "/static/js/2.848de250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bbb342f39ce9e1db024",
+    "url": "/static/js/main.064e52ba.chunk.js"
+  },
+  {
+    "revision": "32573377448cf0e46126",
+    "url": "/static/js/runtime-main.9e5f12bc.js"
+  }
+]);
